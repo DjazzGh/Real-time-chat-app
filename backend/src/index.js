7eef0390b081caa5ca4fe5313ac16343289482c8
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2025-08-25 15:03:25 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "./lib/db.js";
-import cors from 'cors';
 import cookieParser from "cookie-parser";
-import {app, server} from "./lib/socket.js";
+import cors from "cors";
 
 import path from "path";
 
-
-
-
-app.use(cors({
-    origin: 'http://localhost:5173', // Frontend origin
-    credentials: true // Allow cookies
-}));
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cookieParser());
-
-dotenv.config();
-
-const PORT= process.env.PORT
-const __dirname = path.resolve();
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+dotenv.config();
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-    });
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-server.listen(PORT,()=>{
-    console.log("Server is running on port ",PORT);
-    connectDB();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
